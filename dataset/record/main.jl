2b7492db1c0c30ad2e4035f58209baee2471415e
gamercreative <akramsiefeddine@gmail.com>
2025-12-20 20:09:53 +0200

    finished basic dataset

--- dataset/record/main.jl
+++ dataset/record/main.jl
@@ -1,6 +1,17 @@
-using JSON
+include("./includes.jl")
 
-fileContent = read("/Users/akramseifeddine/Documents/code/drawer/dataset/strokes.json")
+jsonContent = GetJsonFileContent("/Users/akramseifeddine/Documents/code/drawer/dataset/strokes.json")[1]
 
-jsonContent = JSON.parse(fileContent)[1]
+x = Int16.([stroke[1] for stroke in jsonContent])
+y = Int16.([stroke[2] for stroke in jsonContent])
+penStates = Bool.([stroke[3] for stroke in jsonContent])
+timestamps = Float64.([stroke[4] for stroke in jsonContent])
 
+spaceStrokes = SpaceStroke(x, y, penStates, timestamps)
+
+dx = diff(x)
+dy = diff(y)
+
+strokes = Stroke(dx, dy, penStates[2:end], timestamps[2:end])
+
+ValidateStroke(strokes)
