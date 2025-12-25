28c823614b69f6b2c4ba1a683a486e9069cad1f9
akram <akram@akrams-MacBook-Pro.local>
2025-12-25 21:19:30 +0200

    working one style

--- dataset/clean/main.jl
+++ dataset/clean/main.jl
@@ -1,14 +1,13 @@
 include("./includes.jl")
 
-jsonContent = GetJsonFileContent("/Users/akramseifeddine/Documents/code/drawer/dataset/strokes.json")
+jsonContent = GetJsonFileContent("dataset/strokes.json")
 
 strokeSequences::Vector{Stroke} = Stroke[]
+
 for strokeSequence in jsonContent
     stroke = ExtractStrokeSequence(strokeSequence)
     ValidateStroke(stroke)
     push!(strokeSequences, stroke)
 end
-
-
 
 SaveStrokes("dataset/ready.json",strokeSequences)
