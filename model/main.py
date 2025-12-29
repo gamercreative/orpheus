4632704772ce7b1e2b32eea1bdd7c937dc2e1430
gamercreative <akramsiefeddine@gmail.com>
2025-12-29 15:34:34 +0200

    added pen state and dt

--- model/main.py
+++ model/main.py
@@ -19,9 +19,9 @@ with open("dataset/ready.json","r") as f:
 
 sequences = []
 for stroke in data:
-    seq = list(zip(stroke["dx"],stroke["dy"]))
+    seq = list(zip(stroke["dx"],stroke["dy"],stroke["pen_state"],stroke["dt"]))
     sequences.append(seq)
-    
+
 tensors = []
 for seq in sequences:
     tens = torch.tensor(seq, dtype=torch.float32)
@@ -32,7 +32,7 @@ motor_seq = pad_sequence(tensors, batch_first=True , padding_value=0.0)
 X = motor_seq[:,:-1,:]
 Y = motor_seq[:,1:,:]
 
-base_embed = torch.tensor([1.0, 0.0], dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+base_embed = torch.tensor([1.0, 0.0, 0.0, 0.0], dtype=torch.float32).unsqueeze(0).unsqueeze(0)
 print(base_embed.shape)
 
 base_embed = base_embed.repeat(Y.size(0),1,1)
@@ -49,10 +49,10 @@ X = torch.cat([base_embed,X],dim=1)
 class DrawerLSTM(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.input_size = 2
+        self.input_size = 4
         self.hidden_size = 128
         self.num_layers = 3
-        self.output_size = 2
+        self.output_size = 4
         
         self.lstm = nn.LSTM(input_size=self.input_size, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True)
         self.norm = nn.LayerNorm(self.hidden_size)
@@ -85,19 +85,6 @@ for epoch in range(50):
     if epoch % 20 == 0:
         print(f"Epoch {epoch} - Loss: {loss.item()}")
 
-# def DrawOut(model,inp):
-#     model.eval()
-#     out = model(inp).detach().cpu()
-#     motor_seq = out[0]
-
-#     # convert deltas to absolute coordinates
-#     x = torch.cumsum(motor_seq[:, 0], dim=0)
-#     y = torch.cumsum(motor_seq[:, 1], dim=0)
-
-#     plt.plot(x, y)
-#     plt.axis('equal')
-#     plt.show()
-
 def DrawOut(model,lett,steps):
     model.eval()
     h0 = torch.randn(model.num_layers,1,model.hidden_size)
@@ -110,7 +97,7 @@ def DrawOut(model,lett,steps):
         out = model.norm(out)
         out = model.output_layer(out)
         
-        delta = out[:,-1:,:] + torch.rand_like(out[:,-1,:]) * 0.2
+        delta = out[:,-1:,:] + torch.rand_like(out[:,-1,:]) * 0.15
         motor_seq.append(delta.squeeze(0))
         inp = delta
 
@@ -124,14 +111,6 @@ def DrawOut(model,lett,steps):
     plt.axis('equal')
     plt.show()
 
-letter_embed = torch.tensor([1.0,0.0]).unsqueeze(0).unsqueeze(1)
-DrawOut(model,letter_embed,80)
-DrawOut(model,letter_embed,80)
-
-# Rand = torch.rand_like(X)
-# X = X + Rand
-# DrawOut(model,X)
-
-# Rand = torch.rand_like(X)
-# X = X + Rand
-# DrawOut(model,X)
+letter_embed = torch.tensor([1.0,0.0,0.0,0.0]).unsqueeze(0).unsqueeze(1)
+DrawOut(model,letter_embed,30)
+DrawOut(model,letter_embed,30)
