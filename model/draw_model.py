600dc99b2e913ecda15d25ad0b9f18aeeb035c70
gamercreative <akramsiefeddine@gmail.com>
2025-12-29 18:32:35 +0200

    debugging model

--- model/draw_model.py
+++ model/draw_model.py
@@ -24,27 +24,31 @@ class DrawerLSTM(nn.Module):
         out = self.norm(out)
         
         return self.output_layer(out)
-    
+
 def DrawOut(model,lett,steps):
     model.eval()
-    h0 = torch.randn(model.num_layers,1,model.hidden_size)
-    c0 = torch.randn(model.num_layers,1,model.hidden_size)
+    h0 = torch.zeros(model.num_layers,1,model.hidden_size)
+    c0 = torch.zeros(model.num_layers,1,model.hidden_size)
     inp = lett
 
     motor_seq = []
-    for _ in range(steps):
+    for step in range(steps):
         out,(h0,c0) = model.lstm(inp,(h0,c0))
         out = model.norm(out)
         out = model.output_layer(out)
         
-        delta = out[:,-1:,:] + torch.rand_like(out[:,-1,:]) * 0.1
+        # delta = out[:,-1:,:] + torch.rand_like(out[:,-1,:]) * 0.05
+        delta = out[:,-1:,:]
         motor_seq.append(delta.squeeze(0))
         inp = delta
+
 
     motor_seq = torch.stack(motor_seq)
     print(motor_seq.shape)
     # convert deltas to absolute coordinates
     x = torch.cumsum(motor_seq[:,:, 0], dim=0).detach().cpu()
     y = torch.cumsum(motor_seq[:,:, 1], dim=0).detach().cpu()
+    pen = torch.cumsum(motor_seq[:,:, 2], dim=0).detach().cpu()
+    t = torch.cumsum(motor_seq[:,:, 3], dim=0).detach().cpu()
     
-    return x,y
+    return x,y,pen,t
--- model/main.py
+++ model/main.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn as nn
-from torch.nn.utils.rnn import pad_sequence
 import torch.optim as optim
 import matplotlib.pyplot as plt
-import json
 import data, draw_model
 
 # device agonostic code setu
@@ -16,10 +14,21 @@ else:
 
 # load data
 X1,Y1 = data.LoadData("dataset/ready.json")
-X1 = data.AssignStrokeToLetter(X1,Y1,[1.0, 0.0, 0.0, 0.0])
+X1 = data.AssignStrokeToLetter(X1,Y1,[1.0, 0.0, 1.0, 0.0])
 
 X2,Y2 = data.LoadData("dataset/ready2.json")
-X2 = data.AssignStrokeToLetter(X2,Y2,[1.0, 0.0, 1.0, 0.0])
+X2 = data.AssignStrokeToLetter(X2,Y2,[1.0, 0.0, 0.0, 0.0])
+
+end_token = torch.tensor([0.0,0.0,2.0,1.0]).unsqueeze(0).unsqueeze(0)
+
+print(Y1.shape)
+print(end_token.repeat(Y1.size(0),1,1).shape)
+
+let1_end = end_token.repeat(Y1.size(0),1,1)
+let2_end = end_token.repeat(Y2.size(0),1,1)
+
+Y1 = torch.cat([Y1,let1_end],dim=1)
+Y2 = torch.cat([Y2,let2_end],dim=1)
 
 # init
 model = draw_model.DrawerLSTM()
@@ -32,28 +41,33 @@ for epoch in range(200):
     for X_batch, Y_batch in [(X1, Y1), (X2, Y2)]:
         optimizer.zero_grad()
         out = model(X_batch)
-        loss = criterian(out[:,:-1:,:], Y_batch)
+        loss = criterian(out, Y_batch)
         loss.backward()
         optimizer.step()
 
         if epoch % 20 == 0:
             print(f"Epoch {epoch} - Loss: {loss.item()}")
 
-
+time_frame = torch.tensor(range(0,100)).unsqueeze(1)
 # inference
-letter_embed = torch.tensor([1.0,0.0,0.0,0.0]).unsqueeze(0).unsqueeze(1)
-x1,y1 = draw_model.DrawOut(model,letter_embed,30)
-
-letter_embed = torch.tensor([1.0,0.0,0.0,0.0]).unsqueeze(0).unsqueeze(1)
-x2,y2 = draw_model.DrawOut(model,letter_embed,30)
-
-x_max = torch.max(x1)
-
-x2 = x1 + x_max + 20
-
-x = torch.cat([x1,x2])
-y = torch.cat([y1,y2])
-
-plt.plot(x,y)
-plt.axis('equal')
-plt.show()
+while True:
+    letter_embed = torch.tensor([1.0,0.0,0.0,0.0]).unsqueeze(0).unsqueeze(1)
+    x1,y1,p1,t1 = draw_model.DrawOut(model,letter_embed,100)
+    
+    plt.plot(time_frame,p1)
+    plt.plot(time_frame,t1)
+    plt.title("C")
+    plt.axis('equal')
+    plt.show()
+    
+    letter_embed = torch.tensor([1.0,0.0,1.0,0.0]).unsqueeze(0).unsqueeze(1)
+    x2,y2,p2,t2 = draw_model.DrawOut(model,letter_embed,100)
+    
+    plt.plot(time_frame,p2)
+    plt.plot(time_frame,t2)
+    plt.title("B")
+    plt.axis('equal')
+    plt.show()
+    
+    print(torch.max(p1))
+    print(torch.max(p2))
